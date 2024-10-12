d4a2411a91a947f63a5cab5db97ca71d5dde6299
RJayBernales <rainieljevson0@gmail.com>
2024-10-12 12:31:34 +0800

    Added new method updateCardContainer

--- applet-3.js
+++ applet-3.js
@@ -10,7 +10,7 @@ class DataLogger {
         this.logButton.addEventListener('click', () => this.logData());
         this.clearButton.addEventListener('click', () => this.clearLogs());
     }
-    
+
     logData() {
         const timestamp = new Date().toLocaleString();
         this.loggedData.push(timestamp);
@@ -20,5 +20,23 @@ class DataLogger {
     clearLogs() {
         this.loggedData = []; 
         this.updateCardContainer(); 
+    }
+    updateCardContainer() {
+        this.cardContainer.innerHTML = '';
+
+        this.loggedData.forEach(data => {
+
+            const card = document.createElement('div');
+            card.className = 'card mb-2';
+            card.innerHTML = `
+                <div class="card-body">
+                    <h5 class="card-title">Logged Data</h5>
+                    <p class="card-text">${data}</p>
+                </div>
+            `;
+            this.cardContainer.appendChild(card);
+        });
+
+        this.displayLogCount();
     }
 }
