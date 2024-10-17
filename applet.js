47f77c1cae2fc5c0e15fd36087115d431dee9a72
RJayBernales <rainieljevson0@gmail.com>
2024-10-18 07:20:07 +0800

    Added new method renderAppletGallery which renders the data

--- applet.js
+++ applet.js
@@ -11,5 +11,24 @@ class AppletGallery {
         this.renderAppletGallery(this.appletgallery); 
         this.bindSearchEvent();
     }
+    async fetchData() {
+        try {
+            const response = await fetch(this.dataUrl);
+            this.appletgallery = await response.json();
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    }
 
+    renderAppletGallery(appletgallery) {
+        const appletgalleryCont = document.getElementById('appletgalleryCont');
+        appletgalleryCont.innerHTML = appletgallery.map(applet => 
+            `<div class="card" style="width: 18rem;"> 
+               <img src = "${applet.Image}" class="card-img-top image" alt="${applet.Applet_No}">
+                    <h5> ${applet.Applet_No} </h5>
+                    <p> ${applet.Description}</p>
+                    <a href = "${applet.file}" class = "btn btn-primary">Go to Applet </a>  
+            </div>`
+        ).join('');
+    }
 }
