7e80a6126fc63836596f9cc5e798438a39c4a484
RJayBernales <rainieljevson0@gmail.com>
2024-10-18 04:36:22 +0800

    Added a constructor to the class|

--- applet-4.js
+++ applet-4.js
@@ -1,2 +1,13 @@
 class StudentList {
+
+    constructor(dataUrl) {
+        this.dataUrl = dataUrl;
+        this.students = [];
+        this.init();
+    }
+
+
+
 }
+
+const studentList = new StudentList('applet-4.json');
